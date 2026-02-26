8a527fef7f5a51775760035042aba82f4c249148
CrispyChin <intelligantgamer25@gmail.com>
2026-02-26 16:38:23 -0600

    Add project files.

---
+++ portableShaders.cpp
@@ -0,0 +1,120 @@
+#include <iostream>
+// GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+// GLFW
+#include <GLFW/glfw3.h>
+// Other includes
+#include "Shader.h"
+
+// Settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+int main()
+{
+	// initialization of GLFW
+	glfwInit();
+	// Set required options for GLFW
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FOWARD_COMPAT, GL_TRUE);
+
+	// GLFW Window Error Handling
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGLCapstone", nullptr, nullptr);
+	if (window == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// GLEW Error Handling
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "Failed to initalize GLEW" << std::endl;
+		return -1;
+	}
+
+	// Viewport
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+
+	// Build and Compile Shader Program
+	Shader ourShader("shader.vs", "shader.frag");
+
+	// Vertex Data
+	float vertices[] =
+	{
+		// Positions         // Colors
+		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
+		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  
+		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+	};
+	
+	// Vertex Buffers and attribute pointers
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// Bind Vertex Array Object first, then bind and set vertex buffer(s) and sttribute pointers(s)
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Position Attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// Color Attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	// Uncomment this to draw wireframe polygons
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// Render Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		processInput(window);
+		// rendering commands here
+		// Controls the color that is shown when color buffer is cleared
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		// Clears the Color Buffer
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Call Shader Program
+		ourShader.Use();
+		// Bind VAO
+		glBindVertexArray(VAO);
+		// Draw Triangle
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		// Moves the Back Buffer to the Front
+		glfwSwapBuffers(window);
+		// Returns events from the Window
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// Closes the window when escape is pressed
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
---
+++ shaders.cpp
@@ -0,0 +1,202 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// Settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+const char* vertexShaderSource =
+"#version 330 core\n"
+"layout(location = 0) in vec3 position;\n"
+"out vec4 vertexColor;" // Specify a color output to fragment shader
+"void main()\n"
+"{\n"
+"gl_Position = vec4(position, 1.0);\n" // Directly give vec3 to vec4's constructor
+"vertexColor = vec4(0.5f, 0.0f, 0.0f, 1.0f);\n"
+"}\0";
+
+const char* fragmentShaderSource =
+"#version 330 core\n"
+"in vec4 vertexColor;\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vertexColor;\n"
+"}\0";
+
+
+
+int main()
+{
+	// initialization of GLFW
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FOWARD_COMPAT, GL_TRUE);
+
+	// GLFW Window Error Handling
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGLCapstone", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// GLAD Error Handling
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Vertex Shader
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	// Linking Vertex Shader to Vertex Shader Source
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	// Compiling Vertex Shader
+	glCompileShader(vertexShader);
+	// Vertex Shader Error Handling
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" <<
+			infoLog << std::endl;
+	}
+
+	// Fragment Shader
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	// Linking Fragement Shader to Fragment Shader Source
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	// Compiling Fragment Shader
+	glCompileShader(fragmentShader);
+	// Fragment Shader Error Handling
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" <<
+			infoLog << std::endl;
+	}
+
+	// Shader Program
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	// Attach Shaders
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	// Link Shaders to Shader Program
+	glLinkProgram(shaderProgram);
+	// Shader Program Error Handling
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" <<
+			infoLog << std::endl;
+	}
+	// Shader Cleanup
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	// Vertex Data
+	float vertices[] =
+	{
+		// first triangle
+		-0.5f, -0.5f, 0.0f,
+		 0.0f,  0.5f, 0.0f,
+		 0.5f, -0.5f, 0.0f
+	};
+	unsigned int indices[] =
+	{
+		0, 1, 2 // first trinagle
+
+	};
+
+	// Vertex Buffer Object (VBO)
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// Configure Vertex Attributes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Vertex Array Object (VAO)
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	// Bind Vertex Array Object
+	glBindVertexArray(VAO);
+	// Copy Vertices to Array in Buffer for OpenGL to Use
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	// Set Vertex Attribute Pointers
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Element Buffer Object (EBO)
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	// Bind Element Buffer Object
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// Uncomment this to draw wireframe polygons
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// Render Loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// input
+		processInput(window);
+		// rendering commands here
+		// Controls the color that is shown when color buffer is cleared
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		// Clears the Color Buffer
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Call Shader Program
+		glUseProgram(shaderProgram);
+		// Bind VAO
+		glBindVertexArray(VAO);
+		// Draw Triangle
+		glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+
+		// Moves the Back Buffer to the Front
+		glfwSwapBuffers(window);
+		// Returns events from the Window
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+// Closes the window when escape is pressed
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
